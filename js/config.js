fe07be6abfbcc0b3cebaa375ada65e8a6573ccf4
Kellen Proctor <kellenproctor@gmail.com>
2026-05-14 20:20:13 -0700

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-west-1_FdY97b5En', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5s7jff4tu95s9um1ovg63lf3bn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7eh2k52onkrmkhn8v55he4t97o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
